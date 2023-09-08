631cb944d72064a17631f7887dc0484805716395
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-09-08 15:00:46 +0200

    add reusable css classes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primaryColor: '#0067FF',
+                yellowColor: '#FFC3A0',
+                purpleColor: '#D0A3FF',
+                irisBlueColor: '#A3D7FF',
+                headingColor: '#181A1E',
+                textColor: '#4E545F',
+            },
+
+            boxShadow: {
+                panelShadow: 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;',
+            },
+        },
     },
     plugins: [],
 };
