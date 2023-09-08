42c37b27579e46dff4491aaa56f05d1075f88f8a
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-09-08 13:55:04 +0200

    create header, footer, routes and layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
+import './app.css';
+import Layout from './layout/Layout';
+
 function App() {
-  return (
-    <>
-      <h1>Hello world!</h1>
-    </>
-  )
+    return <Layout />;
 }
 
-export default App
+export default App;
