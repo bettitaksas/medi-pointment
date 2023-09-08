49c7ef2e83d8efcf3ea5c040bef222e28b6c9a10
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-09-08 19:51:54 +0200

    make header responsive with BiMenu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from 'react';
 import logo from '../../assets/images/logo.png';
+import userImg from '../../assets/images/avatar-icon.png';
 import { NavLink, Link } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
 
 const navLinks = [
     {
@@ -22,33 +24,89 @@ const navLinks = [
 ];
 
 const Header = () => {
+    const headerRef = useRef(null);
+    const menuRef = useRef(null);
+
+    const stickyHeaderFunc = () => {
+        window.addEventListener('scroll', () => {
+            if (
+                document.body.scrollTop > 80 ||
+                document.documentElement.scrollTop > 80
+            ) {
+                headerRef.current.classList.add('sticky__header');
+            } else {
+                headerRef.current.classList.remove('sticky__header');
+            }
+        });
+    };
+
+    useEffect(() => {
+        stickyHeaderFunc();
+
+        return window.removeEventListener('scroll', stickyHeaderFunc);
+    }, []);
+
+    const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
+
     return (
-        <header className='header flex items-center'>
+        <header ref={headerRef} className='header flex items-center'>
             <div className='container'>
                 <div className='flex items-center justify-between'>
                     {/* ======logo====== */}
                     <div>
-                        <img src={logo} alt='' className='w-24' />
+                        <img src={logo} alt='logo' className='w-24' />
                     </div>
 
-                    {/* ======menu====== */}
-                    <div className='navigation'>
+                    {/* ========== nav menu =========== */}
+                    <div
+                        className='navigation'
+                        ref={menuRef}
+                        onClick={toggleMenu}
+                    >
                         <ul className='menu flex items-center gap-[2.7rem]'>
-                            {
-                                navLinks.map((link, index) => 
+                            {navLinks.map((link, index) => (
                                 <li key={index}>
-                                    <NavLink 
-                                    to={link.path} 
-                                    className={navClass => 
-                                    navClass.isActive 
-                                    ? 'text-primaryColor text-[16px] leading-7 font-[600]' 
-                                    : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'}
+                                    <NavLink
+                                        to={link.path}
+                                        className={(navClass) =>
+                                            navClass.isActive
+                                                ? 'text-[#0067FF] font-[600] text-[16px] leading-7'
+                                                : 'text-textColor font-[500] text-[16px] leading-7'
+                                        }
                                     >
                                         {link.display}
                                     </NavLink>
-                                </li>)
-                            }
+                                </li>
+                            ))}
                         </ul>
+                    </div>
+
+                    {/* ======nav right====== */}
+                    <div className='flex items-center gap-4'>
+                        <div>
+                            <Link to='/'>
+                                <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                                    <img
+                                        src={userImg}
+                                        className='w-full rounded-full'
+                                        alt=''
+                                    />
+                                </figure>
+                            </Link>
+                        </div>
+
+                        <Link to='/login'>
+                            <button
+                                className='bg-primaryColor py-2 px-6 text-white
+                            font-[600] h-[44px] flex items-center justify-center rounded-[50px]'
+                            >
+                                Login
+                            </button>
+                        </Link>
+
+                        <span className='md:hidden' onClick={toggleMenu}>
+                            <BiMenu className='w-6 h-6 cursor-pointer' />
+                        </span>
                     </div>
                 </div>
             </div>
