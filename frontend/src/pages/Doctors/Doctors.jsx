9cb408bcf9bcc7665fca1725da3b9ed4f5e72ffd
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-09-09 20:11:09 +0200

    create find a doctor searchbar

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,5 +1,50 @@
+import Testimonial from '../../components/Testimonial/Testimonial';
+
 const Doctors = () => {
-    return <div>Doctors</div>;
+    return (
+        <>
+            <section className='bg-[#0AA18F42]'>
+                <div className='container text-center'>
+                    <h2 className='heading'>Find a Doctor</h2>
+                    <div className='max-w-[570px] mx-auto mt-[30px] bg-[#0066ff2c] rounded-md flex items-center justify-between '>
+                        <input
+                            className='py-4 pl-4 pr-2 focus:outline-none cursor-pointer w-full bg-transparent placeholder:text-textColor'
+                            type='search'
+                            placeholder='Search by doctor name or specialization'
+                        />
+                        <button className='btn mt-0 rounded-[0px] rounded-r-md'>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className='container'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                        doctors
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className='container'>
+                    <div className='xl:w-[470px] mx-auto'>
+                        <h2 className='heading text-center'>
+                            What our patient say
+                        </h2>
+                        <p className='text__para text-center'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.
+                        </p>
+                    </div>
+
+                    <Testimonial />
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Doctors;
---
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+const DoctorAbout = () => {
+    return (
+        <div>
+            <div>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex gap-2 items-center'>
+                    About of
+                    <span className='text-irisBlueColor font-bold text-[24px] leading-9'>
+                        {name}
+                    </span>
+                </h3>
+                <p className='text__para'>about</p>
+            </div>
+
+            <div className='mt-12'>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex gap-2 items-center'>
+                    Education
+                </h3>
+                <ul className='pt-4 md:p-5'>qualifications</ul>
+            </div>
+
+            <div className='mt-12'>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex gap-2 items-center'>
+                    Experience
+                </h3>
+                <ul className='pt-4 md:p-5 grid sm:grid-cols-2  gap-[30px]'>
+                    experiences
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default DoctorAbout;
