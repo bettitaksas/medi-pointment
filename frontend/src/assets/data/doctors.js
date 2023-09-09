17ca7181e15e3b662d8fbbbbb303b0c054bb2599
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-09-09 15:46:42 +0200

    add doctors list and cards

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,113 @@
+import doctorImg1 from "../images/doctor-img1.png";
+import doctorImg2 from "../images/doctor-img2.png";
+import doctorImg3 from "../images/doctor-img3.png";
+import doctorImg4 from "../images/doctor-img4.png";
+import doctorImg5 from "../images/doctor-img5.png";
+import doctorImg6 from "../images/doctor-img6.png";
+import doctorImg7 from "../images/doctor-img7.png";
+import doctorImg8 from "../images/doctor-img8.png";
+import doctorImg9 from "../images/doctor-img9.png";
+import doctorImg10 from "../images/doctor-img10.png";
+
+export const doctors = [
+    {
+    id: "01",
+    name: "Dr. James Mitchell",
+    specialty: "Cardiologist",
+    avgRating: 4.7,
+    totalRating: 310,
+    photo: doctorImg1,
+    totalPatients: 1650,
+    hospital: "City Heart Clinic, Birmingham",
+  },
+  {
+    id: "02",
+    name: "Dr. Sarah Wilson",
+    specialty: "Pediatrician",
+    avgRating: 4.9,
+    totalRating: 198,
+    photo: doctorImg2,
+    totalPatients: 1200,
+    hospital: "Children's Medical Center, London",
+  },
+  {
+    id: "03",
+    name: "Dr. Juan Carlos Lopez",
+    specialty: "Obstetrician",
+    avgRating: 4.6,
+    totalRating: 420,
+    photo: doctorImg3,
+    totalPatients: 2200,
+    hospital: "Sunrise Maternity Hospital, Madrid",
+  },
+  {
+    id: "04",
+    name: "Dr. David Baker",
+    specialty: "Orthopedic Surgeon",
+    avgRating: 4.7,
+    totalRating: 335,
+    photo: doctorImg4,
+    totalPatients: 1800,
+    hospital: "Healthy Joints Hospital, New York",
+  },
+  {
+    id: "05",
+    name: "Dr. Laura Turner",
+    specialty: "Oncologist",
+    avgRating: 4.9,
+    totalRating: 280,
+    photo: doctorImg5,
+    totalPatients: 1600,
+    hospital: "Hope Cancer Institute, San Francisco",
+  },
+  {
+    id: "06",
+    name: "Dr. Sara Clark",
+    specialty: "ENT Specialist",
+    avgRating: 4.8,
+    totalRating: 250,
+    photo: doctorImg6,
+    totalPatients: 1400,
+    hospital: "Ear & Throat Clinic, Chicago",
+  },
+  {
+    id: "7",
+    name: "Dr. Christopher Williams",
+    specialty: "Psychiatrist",
+    avgRating: 4.9,
+    totalRating: 325,
+    photo: doctorImg7,
+    totalPatients: 1900,
+    hospital: "Mental Wellness Center, Los Angeles",
+  },
+  {
+    id: "8",
+    name: "Dr. Robert Harris",
+    specialty: "Gastroenterologist",
+    avgRating: 4.7,
+    totalRating: 290,
+    photo: doctorImg8,
+    totalPatients: 1550,
+    hospital: "Digestive Health Clinic, Houston",
+  },
+  {
+    id: "9",
+    name: "Dr. Emily Turner",
+    specialty: "Rheumatologist",
+    avgRating: 4.6,
+    totalRating: 270,
+    photo: doctorImg9,
+    totalPatients: 1250,
+    hospital: "Joint Care Hospital, Sydney",
+  },
+  {
+    id: "10",
+    name: "Dr. Candice Brown",
+    specialty: "Endocrinologist",
+    avgRating: 4.8,
+    totalRating: 305,
+    photo: doctorImg10,
+    totalPatients: 1750,
+    hospital: "Endocrine Wellness Center, Toronto",
+  }
+]
