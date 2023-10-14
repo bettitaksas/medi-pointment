696b7caa71b150322d88c9bddce5a953720fe87e
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-10-14 10:48:24 +0200

    update and delete doctor

--- backend/contorller/doctorController.js
+++ backend/contorller/doctorController.js
@@ -22,6 +22,33 @@ export const getOneDoctor = async (req, res) => {
     }
 };
 
+export const getDoctorProfile = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        const user = await Doctor.findById(userId);
+
+        if (!user) {
+            res.status(404).json({ message: 'User not found' });
+        }
+
+        const appointments = await Booking.find({ doctor: userId });
+
+        const { password, ...rest } = user._doc;
+
+        res.status(200).json({
+            success: true,
+            message: 'Successfully ',
+            data: { ...rest, appointments },
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong! cannot get!',
+        });
+    }
+};
+
 export const getAllDoctor = async (req, res) => {
     try {
         const { query } = req.query;
@@ -50,6 +77,49 @@ export const getAllDoctor = async (req, res) => {
         res.status(404).json({
             success: false,
             message: 'Not found',
+        });
+    }
+};
+
+export const updateDoctor = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const updatedDoctor = await Doctor.findByIdAndUpdate(
+            id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: 'Successfully updated',
+            data: updatedDoctor,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'failed to update',
+        });
+    }
+};
+
+export const deleteDoctor = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Doctor.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success: true,
+            message: 'Successfully deleted',
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'Failed to delete',
         });
     }
 };
