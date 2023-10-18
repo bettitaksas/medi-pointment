4f8878c1f59e1a52677be36525fe9c0f7ae61677
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-10-18 16:55:42 +0200

    formating

--- frontend/src/components/Doctors/DoctorsList.jsx
+++ frontend/src/components/Doctors/DoctorsList.jsx
@@ -1,16 +1,16 @@
-import DoctorCard from "./DoctorCard.jsx";
-import { doctors } from "../../assets/data/doctors";
+import DoctorCard from './DoctorCard.jsx';
+import { doctors } from '../../assets/data/doctors';
 
 const DoctorsList = () => {
-return (
-    <>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-          {doctors?.map(doctor => (
-            <DoctorCard doctor={doctor} key={doctor.id} />
-          ))}
-        </div>
-    </>
-  );
+    return (
+        <>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+                {doctors?.map((doctor) => (
+                    <DoctorCard doctor={doctor} key={doctor.id} />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default DoctorsList;
