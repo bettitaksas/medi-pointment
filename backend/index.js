7145e4ccdf8e636682c0ac2d5eb841bcee0e4d3b
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-10-18 13:07:33 +0200

    add middleware

--- backend/index.js
+++ backend/index.js
@@ -35,11 +35,11 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-/* app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/doctors', doctorRoute);
 app.use('/api/v1/reviews', reviewRoute);
-app.use('/api/v1/bookings', bookingRoute); */
+app.use('/api/v1/bookings', bookingRoute);
 
 app.listen(port, () => {
     connectDB();
