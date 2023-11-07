361c43f258b42286c2b3a7f5f6017541a627feed
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-11-07 11:14:37 +0100

    create getDoctorReview endpoint

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,12 +1,12 @@
 import { authenticate, patientAuth } from '../auth/tokenVertify.js';
-import { createReview, getAllReview } from '../controllers/reviewController.js';
+import { createReview, getDoctorReview } from '../controllers/reviewController.js';
 import express from 'express';
 
 const router = express.Router({ mergeParams: true });
 
 router
     .route('/')
-    .get(getAllReview)
+    .get(getDoctorReview)
     .post(authenticate, patientAuth, createReview);
 
 export default router;
